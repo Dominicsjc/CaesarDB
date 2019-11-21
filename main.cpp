422c423b06f82fb385d9e29a4970e034a1268f45
Dominicsjc <dominicsjc@qq.com>
2019-11-20 23:06:19 -0600

    Current courses Demo

--- main.cpp
+++ main.cpp
@@ -14,13 +14,25 @@ int main(int argc, char *argv[]) {
     userSession *loginedStu = login("Linda Smith", "lunch", caesarDB);
     if(loginedStu){
         std::cout << "Login successfully!" << std::endl;
+        std::cout << std::endl;
+
         //Get current courses TODO: rewrite to API
         std::vector<course> curCourses = loginedStu->getCurCourses();
+
+        /*
+         * Current course list Demo
+         */
+        std::cout << "UoSCode      UosName" << std::endl;
+        for(const course &c : curCourses){
+            std::cout << c.first << "     " << c.second << std::endl;
+        }
+        std::cout << std::endl;
 
         //Free and logout
         delete(loginedStu);
     } else{
         std::cout << "Username or passowrd is incorrect! Please try again." << std::endl;
+        std::cout << std::endl;
     }
 
     //Close DB
